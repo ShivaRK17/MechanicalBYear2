f1f197e15e6a4fd7bb50a60548cdea1841703098
ShivaRK17 <99678125+ShivaRK17@users.noreply.github.com>
2022-08-18 19:55:33 +0530

    Add files via upload

---
+++ courses/coursescript.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+let sp = document.querySelector(".sidepanel");
+let sidecon = document.querySelector(".sidecont");
+
+function openmenu() {
+    // sp.style.width = "30vw";
+    sp.style.transform = "rotateY(0deg)";
+}
+function closepanel() {
+    // sp.style.width = "0vw";
+    sp.style.transform = "rotateY(90deg)";
+}
